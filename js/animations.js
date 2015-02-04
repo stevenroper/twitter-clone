798888cd85d6a610ff52d1f9cc3d12aad0ad143a
Steven Roper <steven.m.roper@gmail.com>
2015-02-03 20:20:35 -0700

    Finishing step 2 (showing tweet button) of project.

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+	$('.tweet-compose').on('focus', function() {
+		$(this).css('height', '5em');
+		$('#tweet-controls').show();
+	});
+
+	$('.tweet-compose').on('blur', function() {
+		$('.tweet-compose').css('height', '2.5em');
+		$('#tweet-controls').hide();
+	});
 });
