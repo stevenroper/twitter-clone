2cfcf08b359b91967ce17dca4f8e296d82ebbeae
Steven Roper <steven.m.roper@gmail.com>
2015-02-04 20:53:20 -0700

    Finishing the char count and disabled button aspects.

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
+	
+	//Shows character count & submit button when dashboard tweet box is active
 	$('#tweet-content').children('.tweet-compose').on('focus', function() {
 		$(this).css('height', '5em');
 		$('#tweet-controls').show();
 	});
 
+	//Hides character count & submit button when dashboard tweet box is unactive
 	$('#tweet-content').children('.tweet-compose').on('blur', function() {
 		$('.tweet-compose').css('height', '2.5em');
 		$('#tweet-controls').hide();
+	});
+
+	//Runs the character count aspect of the tweet dashboard
+	$('.tweet-compose').on('keyup', function() {
+		
+		var charsLeft = 140 - $('.tweet-compose').val().length;
+
+		//Tests char count and disables button if charsLeft is less than 0
+		if(charsLeft > 10) {
+			$('#char-count').css('color', '#999');
+			$('button').prop('disabled', false);
+		} else if(charsLeft <= 10 && charsLeft >= 0) {
+			$('#char-count').css('color', 'red');
+			$('button').prop('disabled', false);
+		} else if (charsLeft < 0) {
+			$('button').prop('disabled', true);
+		}
+
+		//Set the character count in the DOM
+		$('#char-count').html(charsLeft);
 	});
 });
