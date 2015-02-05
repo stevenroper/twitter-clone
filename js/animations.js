f1d7ff5d32322afcf8d48e6912c102b0709860de
Steven Roper <steven.m.roper@gmail.com>
2015-02-04 19:51:42 -0700

    Finishing step 2 of this project.

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$('.tweet-compose').on('focus', function() {
+	$('#tweet-content').children('.tweet-compose').on('focus', function() {
 		$(this).css('height', '5em');
 		$('#tweet-controls').show();
 	});
 
-	$('.tweet-compose').on('blur', function() {
+	$('#tweet-content').children('.tweet-compose').on('blur', function() {
 		$('.tweet-compose').css('height', '2.5em');
 		$('#tweet-controls').hide();
 	});
